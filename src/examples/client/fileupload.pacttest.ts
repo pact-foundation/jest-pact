d5d9fcac262280f9ca08383196a19331caf5544f
YOU54F <yousafn@gmail.com>
2019-05-29 01:18:50 +0100

    test: release v0.1.0, test locally, upload ci test results, plus moving test related files into examples

--- src/examples/client/fileupload.pacttest.ts
+++ src/examples/client/fileupload.pacttest.ts
@@ -10,8 +10,10 @@ jestpact.pactWithSuperTest(
   async (provider: any, client: any) => {
     describe("file upload service", () => {
       test("should successfully allow upload of a base 64 encoded pdf", async () => {
+        const pdflocation: string = "./src/examples/sampledata/";
         const pdfname: string = "test-base64.pdf";
-        const pdf = readFileSync(pdfname);
+        const pdfpath: string = `${pdflocation}${pdfname}`;
+        const pdf = readFileSync(pdfpath);
         const body = `----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"test\"\r\n\r\ntest\r\n----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"document\"; filename=\"${pdfname}\"\r\nContent-Type: application/pdf\r\n\r\n${pdf}\r\n----------------------------713166514119664968500586--\r\n`;
         const interaction: InteractionObject = {
           state: "Service is up and healthy",
@@ -49,12 +51,14 @@ jestpact.pactWithSuperTest(
           .post("/upload")
           .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIXVCJ9")
           .field("test", "test")
-          .attach("document", pdfname)
+          .attach("document", pdfpath)
           .expect(201);
       });
       test("should successfully allow upload of binary encoded pdf", async () => {
+        const pdflocation: string = "./src/examples/sampledata/";
         const pdfname: string = "test.pdf";
-        const pdf = readFileSync(pdfname);
+        const pdfpath: string = `${pdflocation}${pdfname}`;
+        const pdf = readFileSync(pdfpath);
         const body = `----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"test\"\r\n\r\ntest\r\n----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"document\"; filename=\"${pdfname}\"\r\nContent-Type: application/pdf\r\n\r\n${pdf}\r\n----------------------------713166514119664968500586--\r\n`;
         const interaction: InteractionObject = {
           state: "Service is up and healthy",
@@ -92,220 +96,9 @@ jestpact.pactWithSuperTest(
           .post("/upload")
           .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIXVCJ9")
           .field("test", "test")
-          .attach("document", pdfname)
+          .attach("document", pdfpath)
           .expect(201);
       });
     });
   }
 );
-
-// const getClient = () => {
-//   const url = `http://localhost:${pactPort}`;
-//   return supertest(url);
-// };
-
-// describe("File Upload service provider pact", () => {
-//   beforeAll(async () => await provider.setup());
-//   afterEach(async () => await provider.verify());
-//   afterAll(async () => await provider.finalize());
-
-//   describe("file upload service", () => {
-//     test("should successfully allow upload of a base 64 encoded pdf", async () => {
-//       const pdfname: string = "test-base64.pdf";
-//       const pdf = readFileSync(pdfname);
-//       const body = `----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"test\"\r\n\r\ntest\r\n----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"document\"; filename=\"${pdfname}\"\r\nContent-Type: application/pdf\r\n\r\n${pdf}\r\n----------------------------713166514119664968500586--\r\n`;
-//       const interaction: InteractionObject = {
-//         state: "Service is up and healthy",
-//         uponReceiving:
-//           "a well formed request with a base 64 encoded pdf to upload",
-//         withRequest: {
-//           method: "POST",
-//           path: "/upload",
-//           headers: {
-//             "Content-Type": term({
-//               generate:
-//                 "multipart/form-data; boundary=--------------------------560782525175769486914756",
-//               matcher:
-//                 "multipart/form-data; boundary=--------------------------[0-9]{24}"
-//             }),
-//             Authorization: term({
-//               generate: "Bearer eyJhbGciOiJIUzI1NiIXVCJ9",
-//               matcher: "Bearer [0-9A-z]{24}"
-//             }),
-//             "Content-Length": like("299")
-//           },
-//           body
-//         },
-//         willRespondWith: {
-//           headers: {
-//             "Content-Type": "application/json"
-//           },
-//           status: 201
-//         }
-//       };
-
-//       await provider.addInteraction(interaction);
-
-//       const client = getClient();
-//       await client
-//         .post("/upload")
-//         .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIXVCJ9")
-//         .field("test", "test")
-//         .attach("document", pdfname)
-//         .expect(201);
-//     });
-//     test("should successfully allow upload of binary encoded pdf", async () => {
-//       const pdfname: string = "test.pdf";
-//       const pdf = readFileSync(pdfname);
-//       const body = `----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"test\"\r\n\r\ntest\r\n----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"document\"; filename=\"${pdfname}\"\r\nContent-Type: application/pdf\r\n\r\n${pdf}\r\n----------------------------713166514119664968500586--\r\n`;
-//       const interaction: InteractionObject = {
-//         state: "Service is up and healthy",
-//         uponReceiving:
-//           "a well formed request with a binary encoded pdf to upload",
-//         withRequest: {
-//           method: "POST",
-//           path: "/upload",
-//           headers: {
-//             "Content-Type": term({
-//               generate:
-//                 "multipart/form-data; boundary=--------------------------560782525175769486914756",
-//               matcher:
-//                 "multipart/form-data; boundary=--------------------------[0-9]{24}"
-//             }),
-//             Authorization: term({
-//               generate: "Bearer eyJhbGciOiJIUzI1NiIXVCJ9",
-//               matcher: "Bearer [0-9A-z]{24}"
-//             }),
-//             "Content-Length": like("299")
-//           },
-//           body
-//         },
-//         willRespondWith: {
-//           headers: {
-//             "Content-Type": "application/json"
-//           },
-//           status: 201
-//         }
-//       };
-
-//       await provider.addInteraction(interaction);
-
-//       const client = getClient();
-//       await client
-//         .post("/upload")
-//         .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIXVCJ9")
-//         .field("test", "test")
-//         .attach("document", pdfname)
-//         .expect(201);
-//     });
-//   });
-
-// });
-
-// const pactPort = 9880;
-// const provider = getProvider({
-//   pactPort,
-//   provider: "file-upload-provider"
-// });
-
-// const getClient = () => {
-//   const url = `http://localhost:${pactPort}`;
-//   return supertest(url);
-// };
-
-// describe("File Upload service provider pact", () => {
-//   beforeAll(async () => await provider.setup());
-//   afterEach(async () => await provider.verify());
-//   afterAll(async () => await provider.finalize());
-
-//   describe("file upload service", () => {
-//     test("should successfully allow upload of a base 64 encoded pdf", async () => {
-//       const pdfname: string = "test-base64.pdf";
-//       const pdf = readFileSync(pdfname);
-//       const body = `----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"test\"\r\n\r\ntest\r\n----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"document\"; filename=\"${pdfname}\"\r\nContent-Type: application/pdf\r\n\r\n${pdf}\r\n----------------------------713166514119664968500586--\r\n`;
-//       const interaction: InteractionObject = {
-//         state: "Service is up and healthy",
-//         uponReceiving:
-//           "a well formed request with a base 64 encoded pdf to upload",
-//         withRequest: {
-//           method: "POST",
-//           path: "/upload",
-//           headers: {
-//             "Content-Type": term({
-//               generate:
-//                 "multipart/form-data; boundary=--------------------------560782525175769486914756",
-//               matcher:
-//                 "multipart/form-data; boundary=--------------------------[0-9]{24}"
-//             }),
-//             Authorization: term({
-//               generate: "Bearer eyJhbGciOiJIUzI1NiIXVCJ9",
-//               matcher: "Bearer [0-9A-z]{24}"
-//             }),
-//             "Content-Length": like("299")
-//           },
-//           body
-//         },
-//         willRespondWith: {
-//           headers: {
-//             "Content-Type": "application/json"
-//           },
-//           status: 201
-//         }
-//       };
-
-//       await provider.addInteraction(interaction);
-
-//       const client = getClient();
-//       await client
-//         .post("/upload")
-//         .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIXVCJ9")
-//         .field("test", "test")
-//         .attach("document", pdfname)
-//         .expect(201);
-//     });
-//     test("should successfully allow upload of binary encoded pdf", async () => {
-//       const pdfname: string = "test.pdf";
-//       const pdf = readFileSync(pdfname);
-//       const body = `----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"test\"\r\n\r\ntest\r\n----------------------------713166514119664968500586\r\nContent-Disposition: form-data; name=\"document\"; filename=\"${pdfname}\"\r\nContent-Type: application/pdf\r\n\r\n${pdf}\r\n----------------------------713166514119664968500586--\r\n`;
-//       const interaction: InteractionObject = {
-//         state: "Service is up and healthy",
-//         uponReceiving:
-//           "a well formed request with a binary encoded pdf to upload",
-//         withRequest: {
-//           method: "POST",
-//           path: "/upload",
-//           headers: {
-//             "Content-Type": term({
-//               generate:
-//                 "multipart/form-data; boundary=--------------------------560782525175769486914756",
-//               matcher:
-//                 "multipart/form-data; boundary=--------------------------[0-9]{24}"
-//             }),
-//             Authorization: term({
-//               generate: "Bearer eyJhbGciOiJIUzI1NiIXVCJ9",
-//               matcher: "Bearer [0-9A-z]{24}"
-//             }),
-//             "Content-Length": like("299")
-//           },
-//           body
-//         },
-//         willRespondWith: {
-//           headers: {
-//             "Content-Type": "application/json"
-//           },
-//           status: 201
-//         }
-//       };
-
-//       await provider.addInteraction(interaction);
-
-//       const client = getClient();
-//       await client
-//         .post("/upload")
-//         .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIXVCJ9")
-//         .field("test", "test")
-//         .attach("document", pdfname)
-//         .expect(201);
-//     });
-//   });
-// });
