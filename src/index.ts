078f0d112ba5dcf0bdbca3ddc34fbd3d0fa1db2a
Timothy Jones <timothy.l.jones@gmail.com>
2020-08-18 18:46:39 +1000

    feat(jest): Add support for Jest v26.x by using jest.setTimeout() (Fixes #197)

--- src/index.ts
+++ src/index.ts
@@ -50,12 +50,21 @@ const jestPactWrapper = (options: JestPactOptions, tests: any) => () => {
     let originalTimeout: number;
 
     beforeAll(() => {
-      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = pactTestTimeout;
+      // Jest's default timeout is 5000, and jest doesn't provide a way of
+      // asking what the current timeout is. In Jest 24 and 25, Jasmine was probably
+      // the test runner, so we can ask Jasmine if it is there. In later versions of
+      // Jest (eg 26 and up), Jasmine may not be defined.
+      // See https://github.com/pact-foundation/jest-pact/issues/197 for discussion
+      //
+      // For now, we just assume that 5000 was the original timeout.
+      // The impact is likely to be small, as `jest.setTimeout()` only works for the
+      // current test file
+      originalTimeout = jasmine ? jasmine.DEFAULT_TIMEOUT_INTERVAL : 5000;
+      jest.setTimeout(pactTestTimeout);
     });
 
     afterAll(() => {
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+      jest.setTimeout(originalTimeout);
     });
 
     tests(setupProvider(applyDefaults(options)));
