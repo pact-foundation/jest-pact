8ba6e6643821bac85c03d317ccf7d205bfa4649c
Timothy Jones <timothy.l.jones@gmail.com>
2020-10-06 23:11:16 +1100

    feat(v3): Add draft withPact interface for V3

---
+++ src/v3/pactwith.v3.test.ts
@@ -0,0 +1,57 @@
+import { V3MockServer } from '@pact-foundation/pact/v3/pact';
+import * as supertest from 'supertest';
+import { pactWith } from './index';
+
+const getClient = (mock: V3MockServer) => supertest(mock.url);
+
+pactWith({ consumer: 'MyConsumer', provider: 'pactWith v3' }, (interaction) => {
+  interaction('pact integration', ({ provider, execute }) => {
+    beforeEach(() =>
+      provider
+        .given('A pet 1845563262948980200 exists')
+        .uponReceiving('A get request to get a pet 1845563262948980200')
+        .withRequest({
+          method: 'GET',
+          path: '/v2/pet/1845563262948980200',
+          headers: { api_key: '[]' },
+        })
+        .willRespondWith({
+          status: 200,
+        }),
+    );
+
+    execute(
+      'should be be able to hide the pact stuff behind the scenes with a port of the users choosing',
+      (mock) =>
+        getClient(mock)
+          .get('/v2/pet/1845563262948980200')
+          .set('api_key', '[]')
+          .expect(200),
+    );
+  });
+
+  interaction('another pact integration', ({ provider, execute }) => {
+    beforeEach(() =>
+      provider
+        .given('No pets exist')
+        .uponReceiving('A get request to get a pet 1845563262948980200')
+        .withRequest({
+          method: 'GET',
+          path: '/v2/pet/1845563262948980200',
+          headers: { api_key: '[]' },
+        })
+        .willRespondWith({
+          status: 404,
+        }),
+    );
+
+    execute(
+      'should be be able to hide the pact stuff behind the scenes with a port of the users choosing',
+      (mock) =>
+        getClient(mock)
+          .get('/v2/pet/1845563262948980200')
+          .set('api_key', '[]')
+          .expect(404),
+    );
+  });
+});
