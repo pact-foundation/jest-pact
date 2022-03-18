93d9a782b99730c4a9d15bcb17fab54012e38798
Yousaf Nabi <yousafn@gmail.com>
2022-03-18 15:39:28 +0000

    ci: Add dry run to publish step

--- scripts/ci/lib/publish.sh
+++ scripts/ci/lib/publish.sh
@@ -13,5 +13,5 @@ echo "--> Releasing version ${VERSION}"
 
 echo "--> Releasing artifacts"
 echo "    Publishing jest-pact@${VERSION}..."
-npm publish --access public --tag latest
+npm publish --access public --tag latest --dry-run
 echo "    done!"
