586da1d64a3a921916ab0afeec3883f6bd4dddc0
Yousaf Nabi <yousafn@gmail.com>
2022-03-18 14:41:38 +0000

    ci: Add install step

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -2,6 +2,8 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)" # Figure out where the script is running
 . "$SCRIPT_DIR"/../lib/robust-bash.sh
 
-yarn run dist:ci
+npm i
+
+npm run dist:ci
 
 npm prune --production
